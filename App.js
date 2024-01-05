c7bcfaae4b9f9e3a19fc8ab2b23254fee3a3fabb
pranay <pranayharishchandra@gmail.com>
2024-01-05 08:52:25 +0530

    header

---
+++ App.js
@@ -0,0 +1,19 @@
+import { Container} from 'react-bootstrap'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>
+        <main className='py-3'>
+        this is new react project for google
+
+        </main>
+      </h1>
+
+    </>
+  )
+}
+
+export default App
