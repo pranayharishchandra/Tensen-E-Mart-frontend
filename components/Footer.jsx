188d5ef1ad574ded4dda126094228b5457a5dceb
pranay <pranayharishchandra@gmail.com>
2024-01-17 18:58:34 +0530

    payment, private routes, pagination done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,18 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from 'react-bootstrap';
 
-import React from 'react'
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
 
-function Footer() {
-  const currentYear = 34
   return (
     <footer>
-        <Container>
-          <Row>
-            <Col className="text-center py-3">
-              <p>
-                NotesMart &copy; {currentYear}
-              </p>
-            </Col>
-          </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            <p>ProShop &copy; {currentYear}</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
-
-export default Footer
+  );
+};
+export default Footer;
